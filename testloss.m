b38bc472df748ab5ec58229e59a023c00fdda202
Shiva Kaul <skkaul@cs.cmu.edu>
2011-04-04 11:59:53 -0400

    basisSet.

--- testloss.m
+++ testloss.m
@@ -3,7 +3,7 @@ fraghat_cfg.template = 'h2';
 fraghat_cfg.basisSet = 'STO-3G';
 fraghat_cfg.par = 1.1;
 frag_cfg = fraghat_cfg;
-frag_cfg.basis = '6-31G**';
+frag_cfg.basisSet = '6-31G**';
 
 load('data1/env.mat');
 experiments = [{env(1), env(2), [1,2], [1,2]};
