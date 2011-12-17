2833108d15a4ea96788a7a0d3f2c9dc3c2ca9e0b
acappiello <acappiel@andrew.cmu.edu>
2011-12-17 01:34:26 -0500

    PCA now plots on a logarithmic scale. Added pca2.m, which operates on the full density matrix.
    Signed-off-by: acappiello <acappiel@andrew.cmu.edu>

--- pca1.m
+++ pca1.m
@@ -6,6 +6,7 @@ load(['data\ethane\ethane2\data3.mat']);
 clear classes;
 load('data\ethane\ethane3\data.mat');
 %% set up the pca data
+hold on;
 dataSize = size( HL );
 for ifrag = 1:dataSize( 1 )
     frag1 = HL{ifrag,1};
@@ -22,9 +23,13 @@ for ifrag = 1:dataSize( 1 )
     for ienv=1:nenv
         X(ienv,:) = X(ienv,:)-means;
     end
-    [coef, score, latent] = princomp(X);
+    [coef, score, latent] = princomp(X, 'econ');
+    LnLatent = log(latent);
+    cutoff = find( latent < 10^-25, 1, 'first' );
+    if size( cutoff, 1 ) > 0
+        LnLatent = LnLatent( 1:cutoff, : );
+    end
     figure( 67 );
-    hold on;
-    plot( latent );
+    plot( LnLatent, 'b' );
 end
 hold off;
