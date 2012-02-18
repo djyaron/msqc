21f1301efb3aaa6d6ab47e8287fdf4231acb7473
David Yaron <yaron@cmu.edu>
2012-02-18 16:03:48 -0500

    Still playing around with model3 fits

--- ethane-anal3.m
+++ ethane-anal3.m
@@ -4,7 +4,7 @@ load('ethane4/ethaneDat.mat');
 %%
 ipar = 7;
 disp(['starting fit on geometry ',num2str(ipar)]);
-m = Model3(LL{ipar,1},LL{ipar,1},LL{ipar,3});
+m = Model3(LL{ipar,1},LL{ipar,2},LL{ipar,3});
 
 mixKE = Mixer(0,1); 
 m.addKEmodDiag(1,1,mixKE);
@@ -13,20 +13,18 @@ m.addKEmodBonded(1,6,[1],[1 2],mixKE);
 m.addKEmodBonded(6,6,[1 2],[1 2],mixKE);
 
 f1 = Fitme;
-f1.addFrag(m,LL{ipar,1});
+f1.addFrag(m,HL{ipar,3});
 f1.exactDensity = 1;
 f1.includeEN = zeros(1,m.natom);
 
-%%
-ic = 0;
-for p = -1:0.25:1
-   ic = ic+1;
-   e1{ic} = f1.errDiffs(p);
-   x1(ic) = p;
-   y1(ic) = norm(e1{ic});
-end
+% ic = 0;
+% for p = -1:0.25:1
+%    ic = ic+1;
+%    e1{ic} = f1.errDiffs(p);
+%    x1(ic) = p;
+%    y1(ic) = norm(e1{ic});
+% end
 
-%%
 nfitpar = m.npar;
 start = zeros(1,nfitpar);
 start(1) = 0.0;
