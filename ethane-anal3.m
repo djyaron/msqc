da07dfe897c4914c87959905c428b2e04875caf4
David Yaron <yaron@cmu.edu>
2012-02-25 13:51:49 -0500

    correlation plots

--- ethane-anal3.m
+++ ethane-anal3.m
@@ -1,7 +1,75 @@
 %% ethaneDat.mat is from top two sections of ethane-anal2.m
 clear classes;
 load('ethane4/ethaneDat.mat');
+%% Correlation plots
+for ipar = 1
+   %set(gca,'NextPlot','replacechildren');
+   %ic = 0;
+   x = LL{ipar,1}.EKE;
+   figure(100+ipar)
+   subplot(3,3,1);
+   plot(x,LL{ipar,1}.EKE,'k.');
+   hold on;
+   plot(x,LL{ipar,2}.EKE,'b.');
+   plot(x,LL{ipar,3}.EKE,'y.');
+   plot(x,HL{ipar,1}.EKE,'r.');
+   plot(x,HL{ipar,2}.EKE,'r.');
+   plot(x,HL{ipar,3}.EKE,'r.');
+   title('KE')
+   %legend('KE','LL2','LL3','HL1','HL2','HL3');
+   %ic = ic+1;
+   %F(ic) = getframe();
+   
+   for iatom = 1:LL{ipar,1}.natom
+      x = LL{ipar,1}.Een(iatom);
+      subplot(3,3,iatom+1);
+      hold off;
+      plot(x,LL{ipar,1}.Een(iatom),'k.');
+      hold on;
+      plot(x,LL{ipar,2}.Een(iatom),'b.');
+      plot(x,LL{ipar,3}.Een(iatom),'y.');
+      plot(x,HL{ipar,1}.Een(iatom),'r.');
+      plot(x,HL{ipar,2}.Een(iatom),'r.');
+      plot(x,HL{ipar,3}.Een(iatom),'r.');
+      title(['EN ',num2str(iatom)])
+      %legend(['EN ',num2str(iatom)],'LL2','LL3','HL1','HL2','HL3');
+      %   ic = ic+1;
+      %   F(ic) = getframe();
+   end
+end
+%movie(F,2,0.3)
+%% Replacing operators
+% What happens if we just replace the KE operator with the narrow and
+% diffuse
+ipar = 1;
+m = Model3(LL{ipar,1},LL{ipar,2},LL{ipar,3});
+
+mixKE = Mixer(0,1); % linear interpolation of entire KE operator
+m.addKEmodDiag(1,1,mixKE);
+m.addKEmodDiag(6,[1,2],mixKE);
+m.addKEmodBonded(1,6,[1],[1 2],mixKE);
+m.addKEmodBonded(6,6,[1 2],[1 2],mixKE);
+
+figure(200)
+x = LL{ipar,1}.EKE;
+plot(x,x,'k.');
+hold on;
+plot(x,LL{ipar,2}.EKE,'b.');
+plot(x,LL{ipar,3}.EKE,'y.');
+sym = {'r.','g.','c.'};
+ic = 0;
+
+for p = -1:1:1
+   m.setPar(p);
+   disp(['solving HF for ',num2str(p)]);
+   m.solveHF;
+   ic = ic+1;
+   plot(x,m.EKE,sym{ic});
+end
+
+
 %%
+
 ipar = 7;
 disp(['starting fit on geometry ',num2str(ipar)]);
 m = Model3(LL{ipar,1},LL{ipar,2},LL{ipar,3});
@@ -34,7 +102,7 @@ pt = lsqnonlin(@f1.errDiffs, start,-limits,limits,options);
 err = f1.errDiffs(pt);
 corrPlot(f1,pt, 0, 800+ipar);
 
-%% loop through parameters, adjusting each separately
+%% Plots versus various parameters
 clear classes;
 load('ethane4/ethaneDat10env.mat');
 
