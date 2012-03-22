70e585a87796637f47f47fc4d53149cbf5df3e5f
David Yaron <yaron@cmu.edu>
2012-03-22 10:01:55 -0400

    handles list of environments and no-environment case, with defaul being to return env=0:nenv

--- @Model3/EKE.m
+++ @Model3/EKE.m
@@ -1,11 +1,12 @@
 function res = EKE(obj,envs)
 % Kinetic energy in environment ienv
 if (nargin < 2)
-   envs = 1:obj.nenv;
+   envs = 0:obj.nenv;
 end
 n = size(envs,2);
 res = zeros(1,n);
-for ienv = 1:n
-   res(ienv) = sum(sum( obj.density(ienv).*obj.KE(ienv) ) );
+for i = 1:n
+   ienv = envs(i);
+   res(i) = sum(sum( obj.density(ienv).*obj.KE(ienv) ) );
 end
 
