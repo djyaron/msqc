b119d2145cc6206f27880402fea78836e9944acc
David Yaron <yaron@cmu.edu>
2012-04-05 00:09:24 -0400

    added variable argument passing, seems ready to go

--- makeFitme.m
+++ makeFitme.m
@@ -1,20 +1,41 @@
-function fitme = makeFitme()
-% location of the data
-dataDir = './datasets';
+function fitme = makeFitme(varargin)
+% Create a fitme object, using data on H2, CH4 and ethane
+% Change default parameters by passing list of key, value pairs, e.g.
+%   fitme('datadir','./mydir','plot',0);
+% keys and default values are:
+%  datadir  "./datasets"     directory with *.mat data files
+%  nhl      1                high level theory (1..3)
+%  plot     1                fitme will plot results when parameters change
+%                            (figure numbers are H2:800 ch4:801 ethane:802
+%  envs     0:20             range of environments to include in the fits
+%  h2       2:7              range of h2 geometries to include (1..7)
+%  ch4      []               range of ch4 geometries to include (1..19)
+%  ethane   []               range of ethane geometries to include (1..7)
+%  kemods   1                include mixers to modify kinetic energy
+%  enmods   1                include mixers to modify elec-nuc interaction
+%  deltarho 1                based charge dependence on charges induced
+%                            by field
+
+% To test parsing of input, use:
+%makeFitme('datadir','./mydata','nhl',2,'plot',0,'envs',1:3,'h2',1:3, ...
+%   'ch4',1:4,'ethane',1:2,'kemods',0,'enmods',0,'deltarho',1)
+
+% location of the data sets
+dataDir = checkForInput(varargin,'datadir','./datasets');
 % The high level theory to fit to (second index on the HL's read from data
 % directory
-nhl = 1;
+nhl = checkForInput(varargin,'nhl',1);
 % plot results as fitting occurs
-doPlot = 1;
+doPlot = checkForInput(varargin,'plot',1);
 % environments to include in the fit
-envs = 0:20; % environments to include in fit
-geomsH2 = []; %2:7;
-geomsCH4 = 1:7; % allowed range is 1:19
-geomsEthane = 1:7; % allowed range is 1:7
+envs = checkForInput(varargin,'envs',0:20); 
+geomsH2 = checkForInput(varargin,'h2',2:7); % allowed range is 1:7
+geomsCH4 = checkForInput(varargin,'ch4',[]); % allowed range is 1:19
+geomsEthane = checkForInput(varargin,'ethane',[]); % allowed range is 1:7
+includeKEmods = checkForInput(varargin,'kemods',1);
+includeENmods = checkForInput(varargin,'enmods',1);
+useDeltaCharges = checkForInput(varargin,'deltarho',1);
 
-includeKEmods = 1;
-includeENmods = 1;
-useDeltaCharges = 1;
 
 % Load data
 LL1 = cell(0,0);
@@ -112,3 +133,13 @@ fitme.includeEN = includeENmods * ones(1,6);
 fitme.setEnvs(envs);
 
 end
+
+function res = checkForInput(varargin,inputName,default)
+idx = find(cellfun(@(x)isequal(lower(x),inputName), varargin));
+if (~isempty(idx))
+   res = varargin{idx+1};
+else
+   res = default;
+end
+end
+
