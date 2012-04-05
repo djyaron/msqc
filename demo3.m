19b41ac351383e145007369ba063185af60ca909
David Yaron <yaron@cmu.edu>
2012-04-05 00:08:42 -0400

    added variable argument passing to makeFitme. Things seem ready to go.

--- demo3.m
+++ demo3.m
@@ -1,9 +1,22 @@
+clear classes;
+
+%% H2 fit
 f1 = makeFitme;
-disp('Starting fit');
+disp('Starting fit for h2');
 start = zeros(1,f1.npar);
-   limits = [4 20 4 20 4 20 4 4 4 20 4 20 4 20 4 4 4 4];
-   options = optimset('DiffMinChange',1.0e-5);
-   pt = lsqnonlin(@f1.err, start,-limits,limits,options);
+%limits = [4 20 4 20 4 20 4 4 4 20 4 20 4 20 4 4 4 4];
+limits = [];
+options = optimset('DiffMinChange',1.0e-5);
+pt = lsqnonlin(@f1.err, start,-limits,limits,options);
+
+%% CH4 and ethane fit
+f1 = makeFitme('ch4',1:7,'ethane',1:7);
+disp('Starting fit ch4 and ethane');
+start = zeros(1,f1.npar);
+%limits = [4 20 4 20 4 20 4 4 4 20 4 20 4 20 4 4 4 4];
+limits = [];
+options = optimset('DiffMinChange',1.0e-5);
+pt = lsqnonlin(@f1.err, start,-limits,limits,options);
 
    
 %    options = LMFnlsq;
