443f6185a0ce44a165f8062a4dd39e4ac7662dc5
David Yaron <yaron@cmu.edu>
2012-04-21 17:25:38 -0400

    Updated to do H2 and CH4 simulataneous fit. only ran first part, so not fully tested

--- multi2.m
+++ multi2.m
@@ -1,24 +1,32 @@
 %% Fitting multiple molecules, using makeFitme
 clear classes;
-% Hydrogen
-% >>>> START WITH CONSTANTS <<<<
-dataDir = 'tmp/h2-1/';
-junk = Mixer(0,1,'junk');
-ke.H = Mixer(0,1,'ke.H');
-ke.Cs = junk;
-ke.Cp = junk;
-ke.HH = Mixer(0,1,'ke.HH');
-ke.CsH = junk;
-ke.CpH = junk;
-ke.CsCs = junk;
-ke.CsCp = junk;
-ke.CpCp = junk;
-en = ke;
-en.H = Mixer(0,1,'en.H');
-en.HH = Mixer(0,1,'en.HH');
+%trainMols  = {'h2',[2 4 6 7]};
+%testMols = {'h2',[1 3 5]};
 
-ftest = makeFitme('h2',[1 3 5],'enstruct',en,'kestruct',ke,'plot',2);
-f1 = makeFitme('h2',[2 4 6 7],'enstruct',en,'kestruct',ke, ...
+% >>>> START WITH CONSTANTS <<<<
+ke.H = Mixer(0,1,'ke.H');
+ke.Cs = Mixer(0,1,'ke.C');
+ke.Cp = ke.Cs;
+ke.HH = Mixer(0,1,'ke.HH');
+ke.CsH = Mixer(0,1,'ke.CH');
+ke.CpH = ke.CsH;
+ke.CsCs = Mixer(0,1,'ke.CC');
+ke.CsCp = ke.CsCs;
+ke.CpCp = ke.CsCs;
+
+en.H = Mixer(0,1,'en.H');
+en.Cs = Mixer(0,1,'en.C');
+en.Cp = en.Cs;
+en.HH = Mixer(0,1,'en.HH');
+en.CsH = Mixer(0,1,'en.CH');
+en.CpH = en.CsH;
+en.CsCs = Mixer(0,1,'en.CC');
+en.CsCp = en.CsCs;
+en.CpCp = en.CsCs;
+
+dataDir = 'tmp/ch4-1/';
+ftest = makeFitme('ch4',[1 3 5],'enstruct',en,'kestruct',ke,'plot',2);
+f1 = makeFitme('ch4',[2 4 6 7],'enstruct',en,'kestruct',ke, ...
    'testFitme',ftest);
 disp('Starting fit for H2');
 limits = [];
@@ -41,17 +49,16 @@ figure(799); saveas(gcf,[dataDir,'error.fig']);
 figure(800); saveas(gcf,[dataDir,'h2-train.fig']);
 figure(810); saveas(gcf,[dataDir,'h2-test.fig']);
 
-% >>>>> ADD CH/BO dependence KE <<<<
-dataDir = 'tmp/h2-2/';
-ke.H.mixType = 2;
-ke.H.par(2) = 0;
-ke.H.fixed(2) = 0;
-ke.HH.mixType = 3;
-ke.HH.par(2) = 0;
-ke.HH.fixed(2) = 0;
+%% >>>>> ADD CH/BO dependence KE <<<<
+dataDir = 'tmp/ch4-2/';
+ke.H.mixType = 2;   ke.H.par(2) = 0;  ke.H.fixed(2) = 0;
+ke.C.mixType = 2;   ke.C.par(2) = 0;  ke.C.fixed(2) = 0;
+ke.HH.mixType = 3;  ke.HH.par(2) = 0; ke.HH.fixed(2) = 0;
+ke.CH.mixType = 3;  ke.CH.par(2) = 0; ke.CH.fixed(2) = 0;
+ke.CC.mixType = 3;  ke.CC.par(2) = 0; ke.CC.fixed(2) = 0;
 
-ftest = makeFitme('h2',[1 3 5],'enstruct',en,'kestruct',ke,'plot',2);
-f1 = makeFitme('h2',[2 4 6 7],'enstruct',en,'kestruct',ke, ...
+ftest = makeFitme('ch4',[1 3 5],'enstruct',en,'kestruct',ke,'plot',2);
+f1 = makeFitme('ch4',[2 4 6 7],'enstruct',en,'kestruct',ke, ...
    'testFitme',ftest);
 if (exist(dataDir,'dir') ~= 7)
    status = mkdir(dataDir);
@@ -71,16 +78,15 @@ figure(800); saveas(gcf,[dataDir,'h2-train.fig']);
 figure(810); saveas(gcf,[dataDir,'h2-test.fig']);
 
 % >>>> ADD CH/BO DEPENDENCE TO EN <<<<
-dataDir = 'tmp/h2-3/';
-en.H.mixType = 2;
-en.H.par(2) = 0;
-en.H.fixed(2) = 0;
-en.HH.mixType = 3;
-en.HH.par(2) = 0;
-en.HH.fixed(2) = 0;
+dataDir = 'tmp/ch4-3/';
+en.H.mixType = 2;   en.H.par(2) = 0;    en.H.fixed(2) = 0;
+en.C.mixType = 2;   en.C.par(2) = 0;    en.C.fixed(2) = 0;
+en.HH.mixType = 3;  en.HH.par(2) = 0;   en.HH.fixed(2) = 0;
+en.CH.mixType = 3;  en.CH.par(2) = 0;   en.CH.fixed(2) = 0;
+en.CC.mixType = 3;  en.CC.par(2) = 0;   en.CC.fixed(2) = 0;
 % 
-ftest = makeFitme('h2',[1 3 5],'enstruct',en,'kestruct',ke,'plot',2);
-f1 = makeFitme('h2',[2 4 6 7],'enstruct',en,'kestruct',ke, ...
+ftest = makeFitme('ch4',[1 3 5],'enstruct',en,'kestruct',ke,'plot',2);
+f1 = makeFitme('ch4',[2 4 6 7],'enstruct',en,'kestruct',ke, ...
    'testFitme',ftest);
 if (exist(dataDir,'dir') ~= 7)
    status = mkdir(dataDir);
@@ -99,41 +105,4 @@ diary off;
 figure(799); saveas(gcf,[dataDir,'error.fig']);
 figure(800); saveas(gcf,[dataDir,'h2-train.fig']);
 figure(810); saveas(gcf,[dataDir,'h2-test.fig']);
-
-%% start with just constants
-ke.H = Mixer(0,1,'ke.H');
-ke.C = Mixer(0,1,'ke.C');
-ke.Cp = ke.C;
-ke.HH = Mixer(0,1,'ke.HH');
-ke.CsH = Mixer(0,1,'ke.CH');
-ke.CpH = ke.CsH;
-ke.CsCs = Mixer(0,1,'ke.CC');
-ke.CsCp = ke.CsCs;
-ke.CpCp = ke.CsCs;
-
-en.H = Mixer(0,1,'en.H');
-en.C = Mixer(0,1,'en.C');
-en.Cp = en.C;
-en.HH = Mixer(0,1,'en.HH');
-en.CsH = Mixer(0,1,'en.CH');
-en.CpH = en.CsH;
-en.CsCs = Mixer(0,1,'en.CC');
-en.CsCp = en.CsCs;
-en.CpCp = en.CsCs;
-
-f1 = makeFitme('h2',[],'ch4',1:7,'enstruct',en,'kestruct',ke);
-disp('Starting fit for ch4');
-start = f1.getPars;
-diary('tmp/ch4-1.diary');
-diary on;
-limits = [];
-options = optimset('DiffMinChange',1.0e-5);
-tic
-pt = lsqnonlin(@f1.err, start,-limits,limits,options);
-clockTime = toc
-pt
-f1.printMixers;
-diary off;
-
-
 
