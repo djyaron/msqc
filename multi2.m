e7a355ad3a07242dd8bf1da0c8c306dd677f9eb4
David Yaron <yaron@cmu.edu>
2012-05-01 10:44:05 -0400

    Improved multi molecule fitting

--- multi2.m
+++ multi2.m
@@ -30,7 +30,7 @@ f1 = makeFitme('h2',[],'ethylene',[2 4 6 7],'enstruct',en,'kestruct',ke, ...
    'testFitme',ftest);
 disp('Starting fit for H2');
 limits = [];
-options = optimset('DiffMinChange',1.0e-5,'TolFun',1.0e-4,'TolX',1.0e-3);
+options = optimset('DiffMinChange',1.0e-5,'TolFun',1.0e-3,'TolX',1.0e-2);
 if (exist(dataDir,'dir') ~= 7)
    status = mkdir(dataDir);
 end
@@ -38,9 +38,6 @@ diary([dataDir,'out.diary']);
 diary on;
 tic
 start = f1.getPars;
-%%
-start = ptsave + 0.02*rand(size(ptsave));
-options = optimset('DiffMinChange',1.0e-5,'TolFun',1.0e-3,'TolX',1.0e-2);
 [pt,resnorm,residual,exitflag,output,lambda,jacobian] = ...
    lsqnonlin(@f1.err, start,-limits,limits,options);
 clockTime = toc
@@ -49,19 +46,19 @@ resnorm
 f1.printMixers;
 diary off;
 figure(799); saveas(gcf,[dataDir,'error.fig']);
-figure(800); saveas(gcf,[dataDir,'train.fig']);
-figure(810); saveas(gcf,[dataDir,'test.fig']);
+figure(803); saveas(gcf,[dataDir,'c2h4-train.fig']);
+figure(813); saveas(gcf,[dataDir,'c2h4-test.fig']);
 
 %% >>>>> ADD CH/BO dependence KE <<<<
-dataDir = 'tmp/ch4-2/';
+dataDir = 'tmp/c2h4-2/';
 ke.H.mixType = 2;   ke.H.par(2) = 0;  ke.H.fixed(2) = 0;
 ke.C.mixType = 2;   ke.C.par(2) = 0;  ke.C.fixed(2) = 0;
 ke.HH.mixType = 3;  ke.HH.par(2) = 0; ke.HH.fixed(2) = 0;
 ke.CH.mixType = 3;  ke.CH.par(2) = 0; ke.CH.fixed(2) = 0;
 ke.CC.mixType = 3;  ke.CC.par(2) = 0; ke.CC.fixed(2) = 0;
 
-ftest = makeFitme('ch4',[1 3 5],'enstruct',en,'kestruct',ke,'plot',2);
-f1 = makeFitme('ch4',[2 4 6 7],'enstruct',en,'kestruct',ke, ...
+ftest = makeFitme('h2',[],'ethylene',[1 3 5],'enstruct',en,'kestruct',ke,'plot',2);
+f1 = makeFitme('h2',[],'ethylene',[2 4 6 7],'enstruct',en,'kestruct',ke, ...
    'testFitme',ftest);
 if (exist(dataDir,'dir') ~= 7)
    status = mkdir(dataDir);
@@ -77,19 +74,19 @@ resnorm
 f1.printMixers;
 diary off;
 figure(799); saveas(gcf,[dataDir,'error.fig']);
-figure(800); saveas(gcf,[dataDir,'h2-train.fig']);
-figure(810); saveas(gcf,[dataDir,'h2-test.fig']);
+figure(803); saveas(gcf,[dataDir,'c2h4-train.fig']);
+figure(813); saveas(gcf,[dataDir,'c2h4-test.fig']);
 
-% >>>> ADD CH/BO DEPENDENCE TO EN <<<<
-dataDir = 'tmp/ch4-3/';
+%% >>>> ADD CH/BO DEPENDENCE TO EN <<<<
+dataDir = 'tmp/c2h4-3/';
 en.H.mixType = 2;   en.H.par(2) = 0;    en.H.fixed(2) = 0;
 en.C.mixType = 2;   en.C.par(2) = 0;    en.C.fixed(2) = 0;
 en.HH.mixType = 3;  en.HH.par(2) = 0;   en.HH.fixed(2) = 0;
 en.CH.mixType = 3;  en.CH.par(2) = 0;   en.CH.fixed(2) = 0;
 en.CC.mixType = 3;  en.CC.par(2) = 0;   en.CC.fixed(2) = 0;
 % 
-ftest = makeFitme('ch4',[1 3 5],'enstruct',en,'kestruct',ke,'plot',2);
-f1 = makeFitme('ch4',[2 4 6 7],'enstruct',en,'kestruct',ke, ...
+ftest = makeFitme('h2',[],'ethylene',[1 3 5],'enstruct',en,'kestruct',ke,'plot',2);
+f1 = makeFitme('h2',[],'ethylene',[2 4 6 7],'enstruct',en,'kestruct',ke, ...
    'testFitme',ftest);
 if (exist(dataDir,'dir') ~= 7)
    status = mkdir(dataDir);
@@ -106,6 +103,7 @@ resnorm
 f1.printMixers;
 diary off;
 figure(799); saveas(gcf,[dataDir,'error.fig']);
-figure(800); saveas(gcf,[dataDir,'h2-train.fig']);
-figure(810); saveas(gcf,[dataDir,'h2-test.fig']);
+figure(803); saveas(gcf,[dataDir,'c2h4-train.fig']);
+figure(813); saveas(gcf,[dataDir,'c2h4-test.fig']);
 
+%% Make 
