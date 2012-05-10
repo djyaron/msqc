5bbfd5d4a8b0ff1803a84bd191f1e9f286a1195e
David Yaron <yaron@cmu.edu>
2012-05-09 22:29:25 -0400

    ZIP DATA: data now stored as original gaussian files in zip format. Still reads old data, but only generates new data.

--- @Fragment/findCalc.m
+++ @Fragment/findCalc.m
@@ -7,25 +7,43 @@ function [found,fileprefix] = findCalc(dataPath,Ctarget)
 %  _cfg stores the config structure
 %  _calc stores the data
 
-
-ifile = 0;
-found = false;
-failed = false;
-while (~failed && ~found)
-   ifile = ifile + 1;
-   fileprefix = [dataPath,filesep,Ctarget.template,'_',int2str(ifile)];
-   %disp(['looking for ',fileprefix]);
-   cfgfilename = [fileprefix,'_cfg.mat'];
-   try % try to open the file
-      load(cfgfilename,'Cfile');
-   catch
-      failed = true;
-      %disp('not found');
-   end
-   if (~failed)
+found = 0;
+fileprefix = '';
+allFiles = dir(dataPath);
+nfiles = length(allFiles);
+ifile = 1;
+while (~found && (ifile <= nfiles))
+   fileName = allFiles(ifile).name;
+   if (~isempty(regexp(fileName,'_cfg.mat','once')))
+      %disp(['found ',fileName]);
+      load([dataPath,filesep,fileName],'Cfile');
       if (size(comp_struct(Cfile,Ctarget),1) == 0)
-         found = true;
-         %disp('found it');
+        found = true;
+        fullName = [dataPath,filesep,fileName];
+        fileprefix = strrep(fullName,'_cfg.mat','');
       end
    end
+   ifile = ifile + 1;   
 end
+
+% ifile = 0;
+% found = false;
+% failed = false;
+% while (~failed && ~found)
+%    ifile = ifile + 1;
+%    fileprefix = [dataPath,filesep,Ctarget.template,'_',int2str(ifile)];
+%    %disp(['looking for ',fileprefix]);
+%    cfgfilename = [fileprefix,'_cfg.mat'];
+%    try % try to open the file
+%       load(cfgfilename,'Cfile');
+%    catch
+%       failed = true;
+%       %disp('not found');
+%    end
+%    if (~failed)
+%       if (size(comp_struct(Cfile,Ctarget),1) == 0)
+%          found = true;
+%          %disp('found it');
+%       end
+%    end
+% end
