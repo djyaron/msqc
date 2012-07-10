a3db5ec445975b6e4b545d5d14cfacbb6354334e
David Yaron <yaron@cmu.edu>
2012-07-10 11:51:15 -0400

    analysis of results from hybrid analysis

--- multi6anal.m
+++ multi6anal.m
@@ -1,7 +1,7 @@
 %% Fitting multiple molecules, using makeFitme
 clear classes;
 %myDir = 'T:\matdl\yaron\6-22-12\scaleconst\';
-myDir = 'interpconst\';
+myDir = 'scalehybrid\';
 ftype = 2;
 trainC{1}  = {'h2',[],'ch4',1:17,'envs',1:10};
 testC{1} = {'h2',[],'ch4',1:17,'envs',20:30};
@@ -33,53 +33,55 @@ filePrefix{7} = 'ch4f-c2h6-c2h4';
 
 commonIn = {};
 %
-ofile = fopen('multi6-scale.txt','w');
+ofile = fopen('multih-scale.txt','w');
 molName = {'CH4' 'C2H6' 'C2H4'};
-for iC = [1 2 6] %[1 2 3 4 6 7]
-   for iPar = 1:4
+for iC = [1 2 3 6 7 9] %[1 2 3 4 6 7]
+   for iPar = 1:5
       filePre = filePrefix{iC};
       dataDir = [myDir,filePre,'/fit-',num2str(iPar),'/'];
-      load([dataDir,'all.mat']);
-      % [pt,resnorm,residual,exitflag,output,lambda,jacobian] = ...
-      %  lsqnonlin(@f1.err, start,lowLimits,highLimits,options);
-      [err1 pnum etype] = f1.err(pt);
-%       ftest = makeFitme(testIn{:},commonIn{:},'enstruct1',en, ...
-%          'kestruct',ke,'e2struct',e2,'plot',2);
-%       err2 = ftest.err(pt);
-      for ierr = 1:1
-         if (ierr == 1)
-            err = err1;
-         else
-            err = err2;
-         end
-         err =err*627.509;
-         eke = err(etype==1);
-         eH  = err(etype==11);
-         eC  = err(etype==16);
-         e2  = err(etype==2);
-         tot = norm(err)/sqrt(length(err));
-         ke = norm(eke)/sqrt(length(eke));
-         H = norm(eH)/sqrt(length(eH));
-         C = norm(eC)/sqrt(length(eC));
-         r2 = norm(e2)/sqrt(length(e2));
-         fprintf(ofile,'%s fit %i  ',filePre,iPar);
-         fprintf(ofile,'tot %4.2f ke %4.2f H %4.2f C %4.2f E2 %4.2f \n',...
-            tot, ke, H, C, r2);
-         if (length(unique(pnum)) > 1)
-            for ip = unique(pnum)
-               etot = err(pnum==ip);
-               eke = err(etype==1 & pnum==ip);
-               eH  = err(etype==11 & pnum==ip);
-               eC  = err(etype==16 & pnum==ip);
-               e2  = err(etype==2 & pnum==ip);
-               tot = norm(etot)/sqrt(length(etot));
-               ke = norm(eke)/sqrt(length(eke));
-               H = norm(eH)/sqrt(length(eH));
-               C = norm(eC)/sqrt(length(eC));
-               r2 = norm(e2)/sqrt(length(e2));
-               fprintf(ofile,'>> %s ',molName{ip-800});
-               fprintf(ofile,'tot %4.2f ke %4.2f H %4.2f C %4.2f E2 %4.2f \n',...
-                  tot, ke, H, C, r2);
+      if (exist([dataDir,'all.mat'],'file'))
+         load([dataDir,'all.mat']);
+         % [pt,resnorm,residual,exitflag,output,lambda,jacobian] = ...
+         %  lsqnonlin(@f1.err, start,lowLimits,highLimits,options);
+         [err1 pnum etype] = f1.err(pt);
+         %       ftest = makeFitme(testIn{:},commonIn{:},'enstruct1',en, ...
+         %          'kestruct',ke,'e2struct',e2,'plot',2);
+         %       err2 = ftest.err(pt);
+         for ierr = 1:1
+            if (ierr == 1)
+               err = err1;
+            else
+               err = err2;
+            end
+            err =err*627.509;
+            eke = err(etype==1);
+            eH  = err(etype==11);
+            eC  = err(etype==16);
+            e2  = err(etype==2);
+            tot = norm(err)/sqrt(length(err));
+            ke = norm(eke)/sqrt(length(eke));
+            H = norm(eH)/sqrt(length(eH));
+            C = norm(eC)/sqrt(length(eC));
+            r2 = norm(e2)/sqrt(length(e2));
+            fprintf(ofile,'%s fit %i  ',filePre,iPar);
+            fprintf(ofile,'tot %4.2f ke %4.2f H %4.2f C %4.2f E2 %4.2f \n',...
+               tot, ke, H, C, r2);
+            if (length(unique(pnum)) > 1)
+               for ip = unique(pnum)
+                  etot = err(pnum==ip);
+                  eke = err(etype==1 & pnum==ip);
+                  eH  = err(etype==11 & pnum==ip);
+                  eC  = err(etype==16 & pnum==ip);
+                  e2  = err(etype==2 & pnum==ip);
+                  tot = norm(etot)/sqrt(length(etot));
+                  ke = norm(eke)/sqrt(length(eke));
+                  H = norm(eH)/sqrt(length(eH));
+                  C = norm(eC)/sqrt(length(eC));
+                  r2 = norm(e2)/sqrt(length(e2));
+                  fprintf(ofile,'>> %s ',molName{ip-800});
+                  fprintf(ofile,'tot %4.2f ke %4.2f H %4.2f C %4.2f E2 %4.2f \n',...
+                     tot, ke, H, C, r2);
+               end
             end
          end
       end
