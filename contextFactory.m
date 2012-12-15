dbbc46d62b816d9c0eec39c57d1daaa9beec3fd7
David Yaron <yaron@cmu.edu>
2012-12-14 20:10:14 -0500

    Added weighted fits.

--- contextFactory.m
+++ contextFactory.m
@@ -3,11 +3,12 @@ close all;
 topDir = 'C:/matdl/yaron/dec12a/';
 maxIter = 500;
 
-h2fits = 0;
-combinations = 1;
-costs = [5 10 25 50]; %[0.0001 0.1];
-printDetailsOnLoad = 1;
-
+h2fits = 1;
+combinations = 0;
+costs = []; %[5 10 25 50]; %[0.0001 0.1];
+printDetailsOnLoad = 0;
+weights = 1:10;
+weightProp = 0;
 if (h2fits)
   dsets = cell(1,2);
   dname = cell(1,1);
@@ -22,7 +23,7 @@ if (h2fits)
 else
 % CREATE MODEL SETS
 % dataf = {'ch4rDat','ch4rDat-1c','ch4rDat-diponly','ch4rDat-linrho','ethanerDat','ethylenerDat'};
-dataf = {'ch4rDat','ethanerDat'};% ,'ethylenerDat'};
+dataf = {'ch4rDat'};% ,'ethanerDat'};% ,'ethylenerDat'};
 pnn = [791,792,793];
 dsets = cell(1,2);
 dname = cell(1,1);
@@ -299,6 +300,40 @@ for ipol = 1:length(policies)
             f1.printEDetails(summaryFile);
             ftest.printEDetails(summaryFile);
          end
+      end
+      if (~isempty(weights))
+         for weight = weights
+%             startName = [topDir,filePre,'/all-',num2str(3),'.mat'];
+%             fprintf(1,'LOADING %s for cost %10.5f \n',allName,cost);
+%             fprintf(summaryFile,'LOADING %s for cost %10.5f \n',allName,cost);
+%             load(allName,toSave{:});
+            weightDir = [topDir,filePre,'/all-',num2str(3),'-weight'];
+            if (weightProp)
+               weightDir = [weightDir,'p'];
+            end
+            if (exist(weightDir,'dir') ~= 7)
+               status = mkdir(weightDir);
+            end
+            allName = [weightDir,'/all-',num2str(weight),'.mat'];
+            if (exist(allName,'file'))
+               fprintf(1,'LOADING WEIGHT %10.5f \n',weight);
+               fprintf(summaryFile,'LOADING WEIGHT %10.5f \n',weight);
+               load(allName,toSave{:});
+            else
+               fprintf(1,'STARTING WEIGHT %10.5f \n',weight);
+               fprintf(summaryFile,'STARTING WEIGHT %10.5f \n',weight);
+               f1.setWeights(weight,weightProp);
+               [currentTrainErr,currentPar,currentErr] = ...
+                  contextFit3(f1,ftest,maxIter);
+               save(allName,toSave{:});
+            end
+            str2 = 'context error %12.5f test %12.5f \n';
+            fprintf(1,str2,currentTrainErr,currentErr);
+            fprintf(summaryFile,str2,currentTrainErr,currentErr);
+            f1.printEDetails(summaryFile);
+            ftest.printEDetails(summaryFile);
+         end
+
       end
 
       runTime = toc(ticID)
