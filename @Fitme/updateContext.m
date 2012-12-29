f948f058756c598974ccb15ace19ee2fbf0f342a
David Yaron <yaron@cmu.edu>
2012-12-29 16:12:44 -0500

    Bug fix: updating no-env charge, since we take differences with respect to this.

--- @Fitme/updateContext.m
+++ @Fitme/updateContext.m
@@ -2,6 +2,8 @@ function updateContext(obj)
 
 for imod = 1:length(obj.models)
    mod = obj.models{imod};
+   % do no env, since we are taking charge differences
+   mod.updateContext(0);
    for ienv = obj.envs{imod}
       mod.updateContext(ienv);
    end
