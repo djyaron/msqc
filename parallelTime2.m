65a1f57e68c5ca3b5cb95b8bc455f8234e268ae1
acappiello <acappiel@andrew.cmu.edu>
2013-06-14 18:00:50 -0400

    Fitme.parallel now set automatically.
    Signed-off-by: acappiello <acappiel@andrew.cmu.edu>

--- parallelTime2.m
+++ parallelTime2.m
@@ -1,4 +1,5 @@
 %% Parallel version
+% 409.0881
 iP = 1;
 ftype = 2;
 ke.H = Mixer(iP,1,'ke.H',ftype);
@@ -24,18 +25,19 @@ en.CsCp = en.CsCs;
 en.CpCp = en.CsCs;
 
 e2.H = Mixer(iP,1,'e2.H',ftype);
+
 e2.C = Mixer(iP,1,'e2.C',ftype);
 e2.HH = Mixer(iP,1,'e2.HH',ftype);
 e2.CC = Mixer(iP,1,'e2.CC',ftype);
 e2.CH = Mixer(iP,1,'e2.CH',ftype);
 
 f2 = makeFitme('ch4',1:16,'ethane',1:7,'ethylene',1:7,'envs',0:25,...
-    'enstruct1',en,'kestruct',ke,'e2struct',e2,'verify',1);
+    'enstruct1',en,'kestruct',ke,'e2struct',e2,'verify',0,...
+    'datadir','../msqc/datasets');
 f2.scratchDir = 'I:/';
 p = f2.getPars();
 f2.plot = 0;
 
-f2.parallel = 1;
 f2.parHF = zeros(1,14);
 tic
 err2 = f2.err(p);
